d1b9c86c917b2f1158e557f137b0a241600d88e7
vctecc <vctecc@gmail.com>
2021-09-19 01:05:00 +0300

    add basic message template

--- admin/notify/admin.py
+++ admin/notify/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import MessageTemplate
 
-# Register your models here.
+
+@admin.register(MessageTemplate)
+class MessageTemplate(admin.ModelAdmin):
+    pass
+    # list_display = ('title', )
+    # fields = (
+    #     'title', 'description', 'body'
+    # )
+    # search_fields = ('title', )
