c923ffa12592cf1bf2d9deb7b68bc31c06039d51
vctecc <vctecc@gmail.com>
2021-09-19 01:54:08 +0300

    add base email render

--- senders/notificators/base.py
+++ senders/notificators/base.py
@@ -1,10 +1,19 @@
 import abc
+from jinja2 import Template
 
 
 class BaseNotificator:
+    def __init__(self, db=None):
+        self.db = db
+
     @abc.abstractmethod
     def _send(self, **kwargs):
         pass
+
+    def render_message(self, message_type: str, channel: str,
+                       payload: dict) -> str:
+        template = Template("<strong> hello {{ user_id }} !</strong>")
+        return template.render(**payload)
 
     def _save_history(self):
         # TODO implement me
