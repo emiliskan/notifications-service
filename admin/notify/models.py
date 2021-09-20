bb7d3cba07fbca9a99af4021fa094d8f1e0c1379
vctecc <vctecc@gmail.com>
2021-09-20 23:38:43 +0300

    update message template model

--- admin/notify/models.py
+++ admin/notify/models.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from model_utils.models import TimeStampedModel
@@ -10,13 +11,16 @@ class MessageTemplateType(models.TextChoices):
     websocket = 'websocket'
 
 
+CHANNEL = models.CharField(_('способ связи'),  max_length=20,
+                           choices=MessageTemplateType.choices,
+                           default=MessageTemplateType.email)
+
+
 class MessageTemplate(TimeStampedModel):
     id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4(), editable=False)
-    channel = models.CharField(_('способ связи'),  max_length=20,
-                               choices=MessageTemplateType.choices,
-                               default=MessageTemplateType.email)
-    type = models.CharField(_('тип сообщения'), max_length=80)
-    title = models.CharField(_('название'), max_length=80)
+    channel = CHANNEL
+    type = models.CharField(_('тип сообщения'), max_length=80, default='')
+    sender = models.CharField(_('отправитель'), max_length=80, default='admin')
     description = models.CharField(_('описание'), max_length=255, blank=True)
     body = models.TextField(_('шаблон'))
 
@@ -24,21 +28,20 @@ class MessageTemplate(TimeStampedModel):
         verbose_name = _('шаблон')
         verbose_name_plural = _('шаблоны')
         db_table = 'message_template'
+        unique_together = ('channel', 'type',)
 
     def __str__(self):
-        return self.title
+        return self.type
 
 
-# TODO set all fields editable=False
 class NotifyHistory(models.Model):
-    id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4(), editable=False)
-    service = models.CharField(_('источник'), max_length=80, null=True)
-    channel = models.CharField(_('способ связи'),  max_length=20,
-                               choices=MessageTemplateType.choices,
-                               default=MessageTemplateType.email)
-    type = models.CharField(_('тип сообщения'), max_length=80, null=True)
-    send_time = models.DateField(_('дата отправки'), null=True)
-    body = models.TextField(_('текст сообщения'), blank=True)
+    id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4())
+    service = models.CharField(_('источник'), max_length=80, default='admin')
+    channel = CHANNEL
+    type = models.CharField(_('тип сообщения'), max_length=80, default='email')
+    recipient = models.CharField(_('получатель'), max_length=80, default='nobody@email.com')
+    send_time = models.DateTimeField(_('дата отправки'), default=datetime.now)
+    body = models.TextField(_('текст сообщения'))
 
     class Meta:
         verbose_name = _('история оповещений')
@@ -47,3 +50,6 @@ class NotifyHistory(models.Model):
 
     def __str__(self):
         return self.service
+
+    def has_change_permission(self, request, obj=None):
+        return False
