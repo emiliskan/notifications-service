78305aba711314747e556a23d576f0b0727eaf96
Natalia <maltseva@arrival.com>
2021-09-21 00:06:05 +0300

    basic page to send email

--- admin/notify/admin.py
+++ admin/notify/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.conf.urls import url
-# from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User, Group
+from django_celery_beat.models import PeriodicTask
+
 from .models import MessageTemplate, NotifyHistory
 from .views import SendEmail
 
@@ -15,16 +17,37 @@ class NotifyHistoryAdmin(admin.ModelAdmin):
     pass
 
 
-class MyAdminSite(admin.AdminSite):
+class CustomAdminSite(admin.AdminSite):
+
+    def get_app_list(self, request):
+        app_list = super().get_app_list(request)
+        app_list += [
+            {
+                "name": "Notifications",
+                "app_label": "my_test_app",
+                "models": [
+                    {
+                        "name": "Send email",
+                        "object_name": "send email",
+                        "admin_url": "/admin/send_email",
+                        "view_only": True,
+                    }
+                ],
+            }
+        ]
+        return app_list
 
     def get_urls(self):
-        urls = super(MyAdminSite, self).get_urls()
+        urls = super(CustomAdminSite, self).get_urls()
         custom_urls = [
             url(r'send_email/$', admin.site.admin_view(SendEmail.as_view()), name="send_message")
         ]
-        return urls + custom_urls
+        return custom_urls + urls
 
 
-admin_site = MyAdminSite(name='admin')
-# admin_site.register(User)
-# admin_site.register(Group)
+admin_site = CustomAdminSite(name='admin')
+admin_site.register(User)
+admin_site.register(Group)
+admin_site.register(MessageTemplate)
+admin_site.register(NotifyHistory)
+admin_site.register(PeriodicTask)
--- admin/notify/views.py
+++ admin/notify/views.py
@@ -20,25 +20,7 @@ class SendEmail(FormView):
         if form.is_valid():
             data = form.cleaned_data
             print(data)
-            # celery.send_task('send_email', list(data.values()))
-            return self.form_valid(form)
-
-        return self.form_invalid(form)
-
-
-class SendPeriodicEmail(FormView):
-    template_name = 'send_periodic_email.html'
-    form_class = InputForm
-    success_url = '/admin/'
-
-    def post(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        form.full_clean()
-
-        if form.is_valid():
-            data = form.cleaned_data
-            print(data)
+            celery.send_task('send_email', list(data.values()))
             return self.form_valid(form)
 
         return self.form_invalid(form)
